b46fc08cdca82016217c32210659a9e43b06f3fa
Tim Docker <tim@dockerz.net>
2023-01-14 17:14:29 +1100

    working il9341 demo

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ use defmt_rtt as _;
 use embedded_hal::digital::v2::OutputPin;
 use panic_probe as _;
 
+// Embed the `Hz` function/trait:
+use fugit::RateExtU32;
+
 // Provide an alias for our BSP so we can switch targets quickly.
 // Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
 use rp_pico as bsp;
@@ -17,10 +20,22 @@ use rp_pico as bsp;
 
 use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
-    pac,
+    gpio, pac,
     sio::Sio,
+    spi,
     watchdog::Watchdog,
 };
+
+use embedded_graphics::{
+    mono_font::MonoTextStyle,
+    pixelcolor::Rgb565,
+    prelude::*,
+    primitives::{Circle, PrimitiveStyleBuilder, Rectangle},
+    text::{Baseline, Text, TextStyle},
+};
+
+use display_interface_spi::SPIInterface;
+use ili9341::{Ili9341, Orientation};
 
 #[entry]
 fn main() -> ! {
@@ -54,13 +69,72 @@ fn main() -> ! {
     );
 
     let mut led_pin = pins.led.into_push_pull_output();
+    let mut display = {
+        let cs = pins.gpio13.into_push_pull_output();
+        let dc = pins.gpio15.into_push_pull_output();
+        let reset = pins.gpio14.into_push_pull_output();
+        let _sclk = pins.gpio10.into_mode::<gpio::FunctionSpi>();
+        let _mosi = pins.gpio11.into_mode::<gpio::FunctionSpi>();
+        let _miso = pins.gpio12.into_mode::<gpio::FunctionSpi>();
+
+        let spi = spi::Spi::<_, _, 8>::new(pac.SPI1);
+        let spi = spi.init(
+            &mut pac.RESETS,
+            clocks.peripheral_clock.freq(),
+            16_000_000u32.Hz(),
+            &embedded_hal::spi::MODE_0,
+        );
+        Ili9341::new(
+            SPIInterface::new(spi, dc, cs),
+            reset,
+            &mut delay,
+            Orientation::LandscapeFlipped,
+            ili9341::DisplaySize240x320,
+        )
+        .unwrap()
+    };
+
+    let character_style = MonoTextStyle::new(&profont::PROFONT_24_POINT, Rgb565::WHITE);
+    let text_style = TextStyle::with_baseline(Baseline::Top);
+    let black_fill = PrimitiveStyleBuilder::new()
+        .fill_color(Rgb565::BLACK)
+        .build();
+    let green_fill = PrimitiveStyleBuilder::new()
+        .fill_color(Rgb565::GREEN)
+        .build();
+
+    let test_text = "Pixel Blinky";
+
+    Rectangle::new(Point::new(0, 0), Size::new(320, 240))
+        .into_styled(black_fill)
+        .draw(&mut display)
+        .unwrap();
+
+    Text::with_text_style(test_text, Point::new(60, 0), character_style, text_style)
+        .draw(&mut display)
+        .unwrap();
+
+    let circle_size: u32 = 30;
+    let circle_at = Point::new(
+        ((display.size().width - circle_size) / 2) as i32,
+        ((display.size().height - circle_size) / 2) as i32,
+    );
 
     loop {
         info!("on!");
         led_pin.set_high().unwrap();
+        Circle::new(circle_at, circle_size)
+            .into_styled(green_fill)
+            .draw(&mut display)
+            .unwrap();
+
         delay.delay_ms(500);
         info!("off!");
         led_pin.set_low().unwrap();
+        Circle::new(circle_at, circle_size)
+            .into_styled(black_fill)
+            .draw(&mut display)
+            .unwrap();
         delay.delay_ms(500);
     }
 }
